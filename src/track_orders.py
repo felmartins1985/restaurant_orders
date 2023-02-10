c6b3d518d8ed874fe0db2d84b23b6099845e007f
hp <hp@pop-os.localdomain>
2023-02-10 17:58:08 -0300

    feat: requisito 2 classe TrackOrders

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,22 +1,55 @@
+from src.analyze_log import (
+    find_number_first_food,
+    find_foods_set,
+    find_days_never_go,
+)
+
+
+def set_dict_days(data):
+    order_name_days = {}
+    for _, _, day in data:
+        if day not in order_name_days:
+            order_name_days[day] = 1
+        else:
+            order_name_days[day] += 1
+    return order_name_days
+
+
 class TrackOrders:
-    # aqui deve expor a quantidade de estoque
+    def __init__(self):
+        self.tracks = []
+
     def __len__(self):
-        pass
+        return len(self.tracks)
 
     def add_new_order(self, customer, order, day):
-        pass
+        self.tracks.append([customer, order, day])
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        return find_number_first_food(self.tracks, customer)
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        return find_foods_set(self.tracks, customer)
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        return find_days_never_go(self.tracks, customer)
 
     def get_busiest_day(self):
-        pass
+        days = set_dict_days(self.tracks)
+        most_days_amount = 0
+        most_days_name = str()
+        for key, value in days.items():
+            if value > most_days_amount:
+                most_days_amount = value
+                most_days_name = key
+        return most_days_name
 
     def get_least_busy_day(self):
-        pass
+        days = set_dict_days(self.tracks)
+        worst_days_amount = 1000
+        worst_days_name = str()
+        for key, value in days.items():
+            if value < worst_days_amount:
+                worst_days_amount = value
+                worst_days_name = key
+        return worst_days_name
